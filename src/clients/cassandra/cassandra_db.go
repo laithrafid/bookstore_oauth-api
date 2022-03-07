3932a05997ef5dca063ca50315d677825235187f
Laith <laith@bayt.cloud>
2022-03-06 21:54:49 -0500

    added hosts to cassandra clients

--- src/clients/cassandra/cassandra_db.go
+++ src/clients/cassandra/cassandra_db.go
@@ -1,6 +1,8 @@
 package cassandra
 
 import (
+	"strings"
+
 	"github.com/gocql/gocql"
 	"github.com/laithrafid/bookstore_utils-go/config_utils"
 	"github.com/laithrafid/bookstore_utils-go/logger_utils"
@@ -15,11 +17,12 @@ func init() {
 	if confErr != nil {
 		logger_utils.Error("cannot load config of application:", confErr)
 	}
-
+	hosts := strings.Split(config.CassDBnodes, ",")
 	// Connect to Cassandra cluster:
 	cluster := gocql.NewCluster(config.CassDBSource)
 	cluster.Keyspace = config.CassDBKeyspace
 	cluster.Consistency = gocql.Quorum
+	cluster.Hosts = hosts
 
 	var err error
 	if session, err = cluster.CreateSession(); err != nil {
